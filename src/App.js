7edcfeabbf4eff6a50962fa6725977c54b4112d1
SudoSzune <robin1415@kookmin.ac.kr>
2024-08-02 00:07:46 +0900

    feat: 8/1 App.css 수정, 로그인 이후 첫 페이지 완성

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import LoginPage from './pages/Login/loginPage';
 import SplashScreen from './pages/SplashScreen/splashScreen';
 import LoginLoadingPage from './pages/Loading/loginLoadingPage';
 import Main from './pages/Main/main';
+import SignInPage from './pages/SignIn/signIn';
 const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<SplashScreen />} />
+        <Route path="/" element={<SignInPage />} />
+        <Route path="/SplashScreen" element={<SplashScreen />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/oauth2/kakao" element={<LoginLoadingPage />} />
-        <Route path="/main" element={<Main />} />
+        <Route path="/Main" element={<Main />} />
       </Routes>
     </div>
   );
---
+++ src/pages/SignIn/signIn.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const SignIn = () => {
+  const [value, setValue] = React.useState('');
+
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+  };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#3A70FF'
+      },
+      secondary: {
+        main: '#D9D9D9'
+      }
+    }
+  });
+  return (
+    <div className="flex flex-col h-full w-full">
+      <div className="flex  justify-start">
+        <Logo className="w-[180px] h-[157px]" />
+      </div>
+
+      <div className="flex flex-col justify-between items-center gap-y-20">
+        <Box
+          sx={{ border: 3, borderRadius: 2.5, width: 273, height: 122 }}
+          className="flex justify-center items-center font-semibold "
+        >
+          자신의 직급을 선택해주세요.
+        </Box>
+        <FormControl className="pt-[40px]">
+          <FormLabel id="demo-controlled-radio-buttons-group" />
+          <RadioGroup
+            aria-labelledby="demo-controlled-radio-buttons-group"
+            name="controlled-radio-buttons-group"
+            value={value}
+            onChange={handleChange}
+          >
+            <FormControlLabel value="사장" control={<Radio />} label="사장" />
+            <FormControlLabel value="직원" control={<Radio />} label="직원" />
+          </RadioGroup>
+        </FormControl>
+
+        <ThemeProvider theme={theme}>
+          <Button
+            variant="contained"
+            disableElevation
+            sx={{ width: 146, height: 47, borderRadius: 7.5 }}
+            color="primary"
+            className="text-white font-semibold text-xl"
+          >
+            확인
+          </Button>
+        </ThemeProvider>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
